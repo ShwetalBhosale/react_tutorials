97b1cecc88cb4310c01527aea0c51b8d82f4d506
ShwetalBhosale <53152530+ShwetalBhosale@users.noreply.github.com>
2019-07-22 00:47:03 +0530

    Create Header.component.js

---
+++ src/components/app-main/Header.component.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import '../../App.css';
+
+class Header extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+            <span className = "leftNavBar">
+              <h5 className="leftNav">SB</h5>
+              <h3 className="header-title">React.js Tutorials </h3>
+            </span>
+            <h5 className="loggedIn"><u>Shwetal Bhosale</u></h5>
+        </header>
+        
+      </div>
+    );
+  }
+}
+
+export default Header;
