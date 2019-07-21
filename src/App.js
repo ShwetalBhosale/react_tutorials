093389eb63e2df44263c6e0f0b2cc76d57d9c208
ShwetalBhosale <53152530+ShwetalBhosale@users.noreply.github.com>
2019-07-22 00:44:46 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,1 +1,20 @@
 
+import React, { Component } from 'react';
+import './App.css';
+import Header from './components/app-main/Header.component';
+import Contents from '../src/components/app-main/Contents.component';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Header className="App-header">
+            <h3>React.js Tutorials</h3>
+        </Header>
+        <Contents></Contents>
+      </div>
+    );
+  }
+}
+
+export default App;
